63994e47fff28ffe1bd1a8e4dafe75e7d1164d9f
goodbsm2421 <goodbsm2421@gmail.com>
2021-12-10 22:44:34 +0900

    Typescript: Utility Types

--- 9-ADVANCED/9-9-record.ts
+++ 9-ADVANCED/9-9-record.ts
@@ -4,7 +4,7 @@ type PageInfo = {
 
 type Page = 'home' | 'about' | 'contact';
 
-const nav: Record<Page, PageInfo> = {
+const nav: Record<Page, PageInfo> = { // Record를 사용하면 위에서 정의한 Type을 그대로 가져올 수 있다.
     home: {title: 'home'},
     about: {title: 'about'},
     contact: {title: 'contact'},
